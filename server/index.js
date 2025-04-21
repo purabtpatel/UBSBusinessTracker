123d38cd2d8689852357e66ce126faa65f43a9fb
Purab Patel <purabtpatel24@gmail.com>
2025-04-20 20:40:14 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -213,4 +213,4 @@ function filterUSResults(results) {
 
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
