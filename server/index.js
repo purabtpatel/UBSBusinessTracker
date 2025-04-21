76effbfd3e8b5c07dfcfc5f2168444ee50371601
Purab Patel <purabtpatel24@gmail.com>
2025-04-20 20:24:26 -0400

    change

--- server/index.js
+++ server/index.js
@@ -5,8 +5,6 @@ import { google } from 'googleapis';
 import fetch from 'node-fetch';
 import fs from 'fs';
 import jwt from 'jsonwebtoken';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 
 dotenv.config();
@@ -213,13 +211,6 @@ function filterUSResults(results) {
   return results.filter(place => place.address?.countryCodeISO3 === 'USA');
 }
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
